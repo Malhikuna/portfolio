1d11066eaf4601b2d0c473ad5b2b2d61dce65071
Malhikuna <hikmal767@gmail.com>
2023-12-25 13:11:53 +0700

    memperbarui project 1 portfolio

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ const nav = document.querySelector("nav ul");
 menu.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+const links = document.querySelectorAll(".link");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((ele) => ele.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
