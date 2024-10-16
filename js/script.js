56ac7bdf34269aa604287e7721dfc2200334e225
Malhikuna <hikmal767@gmail.com>
2024-10-16 16:46:10 +0700

    menambahkan :)

--- js/script.js
+++ js/script.js
@@ -20,3 +20,21 @@ links.forEach((link) => {
 gsap.registerPlugin(TextPlugin);
 gsap.to(".ket", { duration: 3, text: "I'm studying at Pasundan University" });
 gsap.to(".hello", { duration: 1, text: "Hello, There!" });
+
+// slides
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2500);
+}
